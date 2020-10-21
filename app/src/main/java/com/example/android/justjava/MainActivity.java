f0882c22cd70f5221128d47784329cc27e282e0d
lokesh boddula <lokesh.immanuel555@gmail.com>
2020-10-21 15:02:57 +0530

    Placing Order opens mail with order details

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -8,9 +8,16 @@
 
 package com.example.android.justjava;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.text.NumberFormat;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,8 +26,13 @@ import androidx.appcompat.app.AppCompatActivity;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity = 0;
+    int quantity = 1;
     int price;
+    String cream;
+    String chocolate;
+    String name;
+    int chocolatePrice;
+    int creamPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +45,20 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void submitOrder(View view) {
-        displayMessage("Ordered Placed!,\nTotal : $"+price+"\nThank You!!");
+        calculatePrice();
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(intent.EXTRA_SUBJECT,"Just Java Order for " + name);
+        intent.putExtra(intent.EXTRA_TEXT,createOrderSummary());
+        if (intent.resolveActivity(getPackageManager()) != null) {
+        startActivity(intent);
+        }
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void display(int quantity) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + quantity);
     }
@@ -47,28 +66,76 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     public void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
     public void increment(View view) {
-         quantity = quantity + 1;
-         display(quantity);
-         price = quantity * 5;
+        if(quantity <=99) {
+            quantity = quantity + 1;
+            display(quantity);
+            price = calculatePrice();
+            displayPrice(price);
+        }
+        else if (quantity == 100){
+            Toast.makeText(this,"you cannot have more than 100 Coffees!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void decrement(View view) {
+        if(quantity > 1 ) {
+            quantity = quantity - 1;
+            display(quantity);
+            price = calculatePrice();
+            displayPrice(price);
+        }
+        else if (quantity == 1){
+            Toast.makeText(this,"you cannot have less than one Coffee!",Toast.LENGTH_SHORT).show();
+        }
+
+        display(quantity);
+        price = calculatePrice();
         displayPrice(price);
 
     }
-    public void decrement(View view) {
-        quantity = quantity - 1;
-        display(quantity);
-         price = quantity * 5;
-        displayPrice(price);
 
+    private int calculatePrice() {
+       return  price =  quantity * ( 5 + chocolatePrice + creamPrice);
+    }
+
+    private String createOrderSummary() {
+        inputsName();
+       return "Ordered Placed!,\nName : "+ name + "\nQuantity : " + quantity + cream + chocolate + "\nTotal : $"+ price +"\nThank You!!";
     }
     /**
      * This method displays the given text on the screen.
      */
-    public void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+
+
+    public void hasWhippedCream(View view) {
+        CheckBox WhippedCream = (CheckBox) findViewById(R.id.cream_checkBox);
+        if(WhippedCream.isChecked()) {
+            cream = "\nAdd Whipped Cream";
+            creamPrice = 1;
+        }
+        else {
+            cream = " false";
+            creamPrice = 0;
+        }
+    }
+
+    public void hasChoclate(View view) {
+        CheckBox Choclate = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        if(Choclate.isChecked()) {
+            chocolate = "\nAdd Choclate";
+            chocolatePrice = 2;
+        }
+        else {
+            chocolate = " false";
+            chocolatePrice = 0;
+        }
+    }
+
+    public void inputsName(){
+        EditText nameTextView = (EditText)findViewById(R.id.editTextTextPersonName);
+        name = nameTextView.getText().toString();
     }
 }
